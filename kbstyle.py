c77defa3f180ea3e78e022c38bec547b4b920861
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-09-12 00:58:07 +0530

    load all premium emoji ids for buttons

--- kbstyle.py
+++ kbstyle.py
@@ -1,13 +1,16 @@
 """Colored inline buttons for Bot API 9.4 / PTB 22.7+."""
 from telegram import InlineKeyboardButton
 
-EMOJI_IDS = [
-    "6057848605601963652",
-    "6124902618574625426",
-    "6124898345082165755",
-    "6125399112499075549",
-    "6197330889765033702",
-]
+try:
+    from emoji_ids import EMOJI_IDS
+except ImportError:
+    EMOJI_IDS = [
+        "6057848605601963652",
+        "6124902618574625426",
+        "6124898345082165755",
+        "6125399112499075549",
+        "6197330889765033702",
+    ]
 
 def btn(text, style=None, icon=None, **kwargs):
     kw = dict(kwargs)
@@ -23,10 +26,10 @@ def btn(text, style=None, icon=None, **kwargs):
         return InlineKeyboardButton(text, **kw)
 
 def blue(text, **kwargs):
-    return btn(text, style="primary", icon=kwargs.pop("icon", EMOJI_IDS[4]), **kwargs)
+    return btn(text, style="primary", icon=kwargs.pop("icon", EMOJI_IDS[4 % len(EMOJI_IDS)]), **kwargs)
 
 def green(text, **kwargs):
-    return btn(text, style="success", icon=kwargs.pop("icon", EMOJI_IDS[1]), **kwargs)
+    return btn(text, style="success", icon=kwargs.pop("icon", EMOJI_IDS[1 % len(EMOJI_IDS)]), **kwargs)
 
 def red(text, **kwargs):
-    return btn(text, style="danger", icon=kwargs.pop("icon", EMOJI_IDS[2]), **kwargs)
+    return btn(text, style="danger", icon=kwargs.pop("icon", EMOJI_IDS[2 % len(EMOJI_IDS)]), **kwargs)
